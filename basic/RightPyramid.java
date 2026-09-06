bf3d4929308d7b2c31b7d5d482fbc05e1d7179ff
m ashraf <muhammadashrafp04@gmail.com>
2026-09-06 23:24:57 +0500

    basic setup of java

---
+++ basic/RightPyramid.java
@@ -0,0 +1,12 @@
+package basic;
+
+public class RightPyramid {
+  public static void main(String[] args) {
+    System.out.println("    *");
+    System.out.println("   **");
+    System.out.println("  ***");
+    System.out.println(" ****");
+    System.out.println("*****");
+  }
+
+}
---
+++ basic/basic.java
@@ -0,0 +1,12 @@
+
+class basicProgram {
+  public static void main(String[] args) {
+    System.out.println("*");
+    System.out.println("**");
+    System.out.println("***");
+    System.out.println("****");
+    System.out.println("*****");
+
+  }
+
+}
