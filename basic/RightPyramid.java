0c3eb955307108e2ac540103c04a0c6532d14fa9
m ashraf <m Ashraf>
2026-09-07 23:26:55 +0500

    vaible in java

--- basic/RightPyramid.java
+++ basic/RightPyramid.java
@@ -1,5 +1,3 @@
-package basic;
-
 public class RightPyramid {
   public static void main(String[] args) {
     System.out.println("    *");
---
+++ variable/Variable.java
@@ -0,0 +1,26 @@
+
+public class Variable {
+  public static void main(String[] args) {
+      // byte      1 byte
+      // short     1 byte
+      // int       4 bytes
+      // long      8 bytes
+      // char      2 bytes
+      // float     4 bytes
+      // double    8 bytes
+    // boolean   1 bit now it used one byte
+    // ************************************************
+   int myNumber = 786;
+   System.out.println(myNumber);
+
+   Boolean is18Above = true;
+   System.out.println(is18Above);
+
+   float marks = 85.5f;
+   System.out.println(marks);
+   
+
+
+  }
+
+}
