c4c529f91719b9803c67846e9b771ae1939ad7ef
m ashraf <m Ashraf>
2026-09-07 23:32:01 +0500

    learn basic escape

---
+++ 3_escape/Escape.java
@@ -0,0 +1,11 @@
+
+ class Escape {
+  public static void main(String[] args) {
+    System.out.println("my name is \"m ashraf\"");
+    System.out.println("my name is \\m ashraf\\");
+    System.out.println("my name is \tm ashraf");
+    System.out.println("my name is \bm ashraf");
+    System.out.println("my name is \nm ashraf");
+  }
+  
+}
