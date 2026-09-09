0436be8b68664f6736d593f155c7e2ca1366b538
m ashraf <muhammadashrafp04@gmail.com>
2026-09-09 22:13:45 +0500

    add some more data in userinput

--- 4_userInput/UserInpt.java
+++ 4_userInput/UserInpt.java
@@ -2,15 +2,28 @@ import java.util.Scanner;
 
 class UserInpt {
   public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
+    // Scanner input = new Scanner(System.in);
 
-    System.out.println("enter you name:");
-    String name = input.nextLine();
+    // System.out.println("enter you name:");
+    // String name = input.nextLine();
 
-    System.out.println("enter you age:");
-    int age = input.nextInt();
+    // System.out.println("enter you age:");
+    // int age = input.nextInt();
 
-    System.out.println("hi" + name + " you are " + age + " year old");
+    // System.out.println("hi" + name + " you are " + age + " year old");
+
+Scanner input = new Scanner(System.in);
+  
+System.out.println("Enter your name: ");
+String name  = input.nextLine();
+System.out.println(name +" welcome to  java world");
+
+System.out.println("Enter a: ");
+int a  = input.nextInt();
+System.out.println("Enter b: ");
+int b  = input.nextInt();
+System.out.println("total is ="+a+b);
+
 
   }
 
