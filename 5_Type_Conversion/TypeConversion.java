af888bad515c2f8d2593e0e20420a7a4e00d0a3d
m ashraf <muhammadashrafp04@gmail.com>
2026-09-09 22:27:10 +0500

    TypeConversion

---
+++ 5_Type_Conversion/TypeConversion.java
@@ -0,0 +1,16 @@
+/**
+ * TypeConversion
+ */
+public class TypeConversion {
+  public static void main(String args[]) {
+    // type conversion and casting
+    // ? 1 Automatic type conversyion/ widening - implicit
+    float myFloat = 5;
+
+    // ? 2 Narrowing - explicit
+
+    int myint = (int) 5.4f;
+    System.out.println(myint);
+  }
+
+}
