ac1c05a23235b1427bf4b3b7eb7e22a11bb16b46
m ashraf <muhammadashrafp04@gmail.com>
2026-09-10 23:07:22 +0500

    welcome to ifelse

---
+++ 7_ControlFlow/IfElse/ConditionalStatment.java
@@ -0,0 +1,20 @@
+
+public class ConditionalStatment {
+public static void main(String[] args) {
+    int number = 10;
+
+    if (number > 0) {
+        System.out.println("The number is positive.");
+    } else if (number < 0) {
+        System.out.println("The number is negative.");
+    } else {
+        System.out.println("The number is zero.");
+    }
+    int age = 18;
+    if(age>=18){
+      System.out.println("you are eligible to vote.");
+    }else{
+      System.out.println("you are not eligible to vote.");
+    }
+   }
+}
